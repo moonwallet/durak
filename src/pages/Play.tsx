808a25f4bb4c90f70eae96e75cb80cdf5c173b3f
iower <iower@tutanota.com>
2024-08-23 00:38:13 +0500

    Play: switch lang

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -3,6 +3,10 @@ import { useTranslation } from 'react-i18next'
 import { Button } from '../kit'
 
 import deck from '../assets/deck.jpg'
+import { ReactComponent as LangRu } from '../assets/langRu.svg'
+import { ReactComponent as LangEn } from '../assets/langEn.svg'
+
+import i18n from '../i18n'
 
 export const Play = () => {
   const { t } = useTranslation()
@@ -24,11 +28,24 @@ export const Play = () => {
           className="mt-10 h-[55px]"
           onClick={() => {}}
         >
-          Create room
+          {t('createRoom')}
         </Button>
       </div>
       <div>
-        ...
+        <div className="mb-[18px]">
+          <Button
+            className="w-[64px] p-3 bg-[#D9D9D91A] rounded-full"
+            onClick={() => { i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en') }}
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-4 h-3 mr-1">
+                {i18n.language === 'ru' && <LangRu />}
+                {i18n.language === 'en' && <LangEn />}
+              </div>
+              <div className="text-[10px] leading-[12px] uppercase">{i18n.language}</div>
+            </div>
+          </Button>
+        </div>
       </div>
     </>
   )
