2c199e30cc00528587d814bab59281b81c8e0bb0
iower <iower@tutanota.com>
2024-08-24 22:06:05 +0500

    Workable Menu

--- src/kit/Menu.tsx
+++ src/kit/Menu.tsx
@@ -1,5 +1,6 @@
 import cx from 'classnames'
 import { useTranslation } from 'react-i18next'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 import { Button } from '../kit'
 
@@ -10,42 +11,43 @@ import { ReactComponent as menuRules } from '../assets/menuRules.svg'
 
 export const Menu = () => {
   const { t } = useTranslation()
+  const navigate = useNavigate()
+  const routerLocation = useLocation()
 
   const items = [
     {
       icon: menuPlay,
       text: t('play'),
-      isActive: true,
-      slug: '/play',
+      pathname: '/play',
     },
     {
       icon: menuFind,
       text: t('find'),
-      isActive: false,
-      slug: '/find',
+      pathname: '/find',
     },
     {
       icon: menuAirdrop,
       text: t('airdrop'),
-      isActive: false,
-      slug: '/airdrop',
+      pathname: '/airdrop',
     },
     {
       icon: menuRules,
       text: t('rules'),
-      isActive: false,
-      slug: '/rules',
+      pathname: '/rules',
     },
   ]
 
   return (
-    <div className="flex items-center bg-[#3C3C435C] border-t border-t-[#FFFFFF08] py-[6px]">
+    <div className="flex items-center bg-[#3C3C435C] border-t border-t-[#FFFFFF08]">
       {items.map((item, i) => (
         <Button
           key={`button-${i}`}
           wrapperClassName="flex-grow basis-0"
-          className={cx(item.isActive ? 'text-main' : 'text-text/50')}
-          onClick={() => {}}
+          className={cx(
+            'py-[6px]',
+            routerLocation.pathname === item.pathname ? 'text-main' : 'text-text/50')
+          }
+          onClick={() => { navigate(item.pathname) }}
         >
           <div className="flex flex-col gap-[2px] items-center justify-center">
             <div className="w-8 h-8">
--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -1,8 +1,23 @@
+import { useTranslation } from 'react-i18next'
+// import { useNavigate } from 'react-router-dom'
+
+import { /* Button, */ Menu } from '../kit'
+
+// import deck from '../assets/deck.jpg'
 
 export const Rules = () => {
+  const { t } = useTranslation()
+  // const navigate = useNavigate()
 
   return (
     <>
+      <div className="Top mt-5 flex flex-col items-center gap-1">
+        {t('Rules content soon...')}
+      </div>
+
+      <div className="Bottom">
+        <Menu />
+      </div>
     </>
   )
 }
