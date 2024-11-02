e98a24ada77e762769c9c901bcb4a9a8786dee31
iower <iower@tutanota.com>
2024-11-02 23:14:53 +0500

    Start game

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -73,7 +73,7 @@ export const Play = () => {
           onClick={createRoom}
           isBusy={isBusy}
         >
-          {t('createRoom')}
+          {t('startGame')}
         </Button>
       </div>
 
